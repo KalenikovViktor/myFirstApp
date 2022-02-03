5364bbdd10f5ee75a12a51dc80e2a90e01e5530b
magavk <magavk@gmail.com>
2022-02-03 12:32:14 +0200

    added RearLine

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -6,6 +6,7 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your name:");
             string name=Console.ReadLine();
             Console.WriteLine($"Hello {name}!");
         }
