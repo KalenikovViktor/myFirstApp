7b7b873df23f0324d0e93c4ac3493f4de4f67882
magavk <magavk@gmail.com>
2022-02-06 13:49:16 +0200

    homeworkLesson4

---
+++ FirstApp/hmLesson4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace hmLesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            string input;
+            int result = 0;
+            bool isParse = false;
+            //МОГУ СДЕЛАТЬ ЧТОБЫ ПРИ НЕВЕРНОМ ВВОДЕ ПРОГРАММА ЗАВЕРШАЛАСЬ,
+            //НО ПОМОЕМУ ЭТО НЕАДЕКВАТНО, БУДЕТ ПРОСИТЬ ПОВТОРНО ВВЕСТИ ЧИСЛО
+            do
+            {
+                Console.Write("Enter value of X:");
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out int buf))
+                {
+                    isParse = true;
+                    x = Int32.Parse(input);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("!!!Need input integer numbers!!!");
+                    Console.ResetColor();
+                }
+            }
+            while (!isParse);
+            isParse = false;
+            do
+            {
+                Console.Write("Enter value of Y:");
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out int buf))
+                {
+                    isParse = true;
+                    y = Int32.Parse(input);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("!!!Need input integer numbers!!!");
+                    Console.ResetColor();
+                }
+            }
+            while (!isParse);
+            if (x != y)
+            {
+                for (int i = Math.Min(x, y); i <= Math.Max(x, y); i++)
+                {
+                    result += i;
+                }
+            }
+            else
+            {
+                result = x;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
