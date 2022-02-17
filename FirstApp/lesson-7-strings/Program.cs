c720a4561d3b9606798e2c2a595d9881081ad906
magavk <magavk@gmail.com>
2022-02-17 15:24:20 +0200

    lesson-7-arrays

---
+++ FirstApp/lesson-7-strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson_7_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str=Console.ReadLine();
+            string str2="";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    str2+=char.ToLower(str[i]);
+                }
+                else if (char.IsLower(str[i]))
+                {
+                    str2+=char.ToUpper(str[i]);
+                }
+            }
+            Console.WriteLine(str2);
+        }
+    }
+}
